a797c9e262199b17e0ef2d5626a420b114974009
unknown <masaki.tadakatsu@gmail.com>
2022-09-08 16:37:53 +0900

    add 0002

---
+++ aoj0002.c
@@ -0,0 +1,22 @@
+// AOJ Volume 0 Problem 0002
+
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+    int num, num1, num2;
+    int digits;
+    
+    while (scanf("%d%d", &num1, &num2) != EOF){
+        num = num1 + num2;
+        digits = 0;
+        while (num != 0){
+            num /= 10;
+            digits++;
+        }
+        printf("%d\n", digits);
+    }
+    
+    return (0);
+}
